6f464d6cd8346bfb5064c397455ce32111e48525
Arlosaid <adroal08@gmail.com>
2022-06-01 17:44:55 -0600

    se agrega pagina de nutricion

---
+++ js/calcular-imc.js
@@ -0,0 +1,52 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  pesoEsValido = validarPeso(peso);
+  alturaEsValida = validarAltura(altura);
+
+  if (!pesoEsValido) {
+    tdImc.textContent = "Peso incorrecto";
+    pesoEsValido = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
+  if (!alturaEsValida) {
+    tdImc.textContent = "Altura incorrecta";
+    alturaEsValida = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
+
+  if (pesoEsValido && alturaEsValida) {
+    tdImc.textContent = calcularIMC(peso, altura);
+  }
+}
+
+function calcularIMC(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
+
+function validarPeso(peso) {
+  if (peso >= 0 && peso < 1000) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validarAltura(altura) {
+  if (altura >= 0 && altura < 3.0) {
+    return true;
+  } else {
+    return false;
+  }
+}
